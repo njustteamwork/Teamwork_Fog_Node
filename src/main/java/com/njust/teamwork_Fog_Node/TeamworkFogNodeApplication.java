a5af627c8054b996d4aa475bcfb60d390ab8f33d
Moriarty007 <42855318+Moriarty007@users.noreply.github.com>
2019-11-09 19:02:50 +0800

    修改了包名，明确使用场景

--- src/main/java/com/njust/teamwork_Fog_Node/TeamworkFogNodeApplication.java
+++ src/main/java/com/njust/teamwork_Fog_Node/TeamworkFogNodeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TeamworkServerApplication {
+public class TeamworkFogNodeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TeamworkServerApplication.class, args);
+		SpringApplication.run(TeamworkFogNodeApplication.class, args);
 	}
 
 }
